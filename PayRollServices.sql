28e6493b48aa44e4831d7dcb6b2df7d95acb7e2c
yashwanthgh <yugnasura@gmail.com>
2024-04-23 15:20:42 +0530

    Added: Ability to create employee payroll data in the payroll service database

--- PayRollServices.sql
+++ PayRollServices.sql
@@ -18,4 +18,12 @@ use Payroll_Service;
 
 create table Employee_Payroll (Id int primary key identity (1,1), Name nvarchar(20), Start date);
 
+-- Insirting ito the Employee Payroll table
+
+insert into Employee_Payroll values 
+	   ('John Doe', '2023-01-15'),
+       ('Jane Smith', '2023-02-28'),
+       ('Alice Johnson', '2023-03-10'),
+       ('Bob White', '2023-04-05');
+
 
