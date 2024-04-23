71e7ba18019f793d6076c4d740db93f971e6b244
yashwanthgh <yugnasura@gmail.com>
2024-04-23 15:07:49 +0530

    Added: Ability to create a payroll service database

--- PayRollServices.sql
+++ PayRollServices.sql
@@ -2,3 +2,15 @@
 
 -- Welcome to payroll service
 
+-- Creating Payroll_Service database.
+
+create Database Payroll_Service;
+
+-- Displaying all the databses in the server (Local).
+
+select * from sys.databases;
+
+-- Using the Payroll_Service database.
+
+use Payroll_Service;
+
